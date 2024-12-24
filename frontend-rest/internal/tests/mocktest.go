521502c7ef1918448815ad34eb24a5f09184f77d
GE:KKA <kosserdyuk@gmail.com>
2024-12-24 22:54:52 +0500

    last commit on my old codebase

--- frontend-rest/internal/tests/mocktest.go
+++
@@ -1,9 +0,0 @@
-package testutils
-
-//go:generate mockery --name=Testutils --dir=. --outpkg=mmk --filename=mock_testutils.go --output=./mocks/testutils --structname MockTestutils --inpackage=true
-type Testutils interface {
-}
-
-//go:generate mockery --name=Interface --dir=. --structname MockInterface --filename=mock_interface.go --inpackage=true
-type Interface interface {
-}
